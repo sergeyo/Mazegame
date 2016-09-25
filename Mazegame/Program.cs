d23cb299c5fbbb1ea268195658c5ab3aa5c544f1
excell <excell@VDEVELOP>
2016-09-25 17:31:16 +0300

    initial commit

---
+++ Mazegame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mazegame.Control;
+
+namespace Mazegame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DungeonMaster theDm = new DungeonMaster(new HardCodedData(), new SimpleConsoleClient());
+            theDm.RunGame();
+        }
+    }
+}
